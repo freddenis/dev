4912954e2699b47c5ab1ef0f185949337d8e5bbd
Fred Denis <fred.denis3@gmail.com>
2020-04-04 21:25:05 +1000

    from oracle scripts

---
+++ ExadataStatus.sh
@@ -0,0 +1,1 @@
+This script has been renamed to rac-status.sh : https://github.com/freddenis/oracle-scripts/blob/master/rac-status.sh
---
+++ exa_on_all_db.sh
@@ -0,0 +1,3 @@
+
+- Has been renamed rac-on_all_db.sh
+- The code is available here : https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-on_all_db.sh
